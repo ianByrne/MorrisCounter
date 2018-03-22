f842e8a569ddb9ed230eefb2f1d6f03cfde348ab
Ian Byrne <ian.byrne@burnsie.com.au>
2018-03-22 05:18:06 +0000

    Add project files.

---
+++ MorrisCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MorrisCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
